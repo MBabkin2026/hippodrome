2b89b702c9b3f9d56a75805529bdd8df3730b82e
Babkin Maksim <mak.babkin.2025@mail.ru>
2025-11-27 20:18:14 +0300

    MOD Hippodrome class

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -1,3 +1,8 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -6,16 +11,20 @@ import static java.util.Objects.isNull;
 
 public class Hippodrome {
 
+    private static final Logger logger = LogManager.getLogger(Hippodrome.class);
     private final List<Horse> horses;
 
     public Hippodrome(List<Horse> horses) {
         if (isNull(horses)) {
+            logError("Horses list is null");
             throw new IllegalArgumentException("Horses cannot be null.");
         } else if (horses.isEmpty()) {
+            logError("Horses list is empty");
             throw new IllegalArgumentException("Horses cannot be empty.");
         }
 
         this.horses = horses;
+        logDebug("Создание Hippodrome, лошадей [" + horses.size() + "]");
     }
 
     public List<Horse> getHorses() {
@@ -31,4 +40,16 @@ public class Hippodrome {
                 .max(Comparator.comparing(Horse::getDistance))
                 .get();
     }
+
+    private void logError(String message) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS"));
+        logger.info(timestamp + " ERROR Hippodrome: " + message);
+    }
+
+    private void logDebug(String message) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS"));
+        logger.info(timestamp + " DEBUG Hippodrome: " + message);
+    }
+
 }
+
